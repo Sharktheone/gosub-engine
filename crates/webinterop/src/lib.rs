88fd27e717db9d7cf0398b7b8cf75ad8ad2c76ef
Sharktheone <sharktheone@proton.me>
2024-01-04 06:01:55 +0100

    it needs another macro...

--- crates/webinterop/src/lib.rs
+++ crates/webinterop/src/lib.rs
@@ -1,31 +1,37 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use quote::__private::ext::RepToTokensExt;
+use std::env;
 
-use syn::{ItemStruct, LitStr, Meta};
+use syn::{ItemImpl, ItemStruct, LitStr, Meta, Path};
 use syn::__private::ToTokens;
 
-use crate::items::{Executor, Field, Function, PropertyOptions};
+use crate::items::{Executor, Field, Function};
 
 mod items;
+mod js_types;
 
 #[proc_macro_attribute]
 pub fn web_interop(attr: TokenStream, item: TokenStream) -> TokenStream {
     let mut fields: Vec<Field> = Vec::new();
     let mut functions: Vec<Function> = Vec::new();
 
-    let mut input: ItemStruct = syn::parse_macro_input!(item);
+
+    let item2 = item.clone();
+    let mut input: ItemStruct = syn::parse_macro_input!(item2);
+
 
     for field in &mut input.fields {
         let mut remove_attrs = Vec::new();
 
         for (index, attr) in field.attrs.iter().enumerate() {
             if attr.path().is_ident("property") {
-                let mut options = PropertyOptions {
+                let mut f = Field {
+                    name: field.ident.as_ref().unwrap().to_string(),
                     executor: Executor::Both,
-                    rename: None,
+                    field_type: field.ty.clone(),
                 };
+
 
                 //rename = "____", js => rename to name and it is a js only property
                 //rename = "____", wasm => rename to name and it is a wasm only property
@@ -42,13 +48,13 @@ pub fn web_interop(attr: TokenStream, item: TokenStream) -> TokenStream {
                                 path if path.is_ident("rename") => {
                                     let lit: LitStr = meta.value()?.parse()?;
 
-                                    options.rename = Some(lit.value());
+                                    f.name = lit.value();
                                 }
                                 path if path.is_ident("js") => {
-                                    options.executor = Executor::JS;
+                                    f.executor = Executor::JS;
                                 }
                                 path if path.is_ident("wasm") => {
-                                    options.executor = Executor::WASM;
+                                    f.executor = Executor::WASM;
                                 }
                                 _ => Err(syn::Error::new_spanned(attr, "Unknown attribute in property attribute"))?
                             }
@@ -63,6 +69,8 @@ pub fn web_interop(attr: TokenStream, item: TokenStream) -> TokenStream {
 
 
                 remove_attrs.push(index);
+
+                fields.push(f);
             }
         }
 
@@ -71,5 +79,33 @@ pub fn web_interop(attr: TokenStream, item: TokenStream) -> TokenStream {
         }
     }
 
+    // let impls: ItemImpl = syn::parse_macro_input!(item);
+    //
+    // dbg!(impls);
+
+
     input.into_token_stream().into()
+}
+
+
+#[proc_macro_attribute]
+pub fn web_fns(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let item2 = item.clone();
+
+    let mut input: ItemImpl = syn::parse_macro_input!(item2);
+
+    dbg!(input.clone());
+
+    item
+}
+
+fn get_crate() -> Path {
+    let mut name = env::var("CARGO_PKG_NAME").unwrap();
+    if name == "gosub-engine" {
+        name = "crate".to_string();
+    }
+
+    let name = name.replace('-', "_");
+
+    syn::parse_str::<Path>(&name).unwrap()
 }
--- src/webinterop.rs
+++ src/webinterop.rs
@@ -4,16 +4,22 @@
 
 #[cfg(test)]
 mod tests {
-    use webinterop::web_interop;
+    use std::collections::HashMap;
+    use webinterop::{web_fns, web_interop};
 
     #[web_interop]
     struct TestStruct {
 
         #[property]
         field: i32,
+
+        #[property]
+        field2: HashMap<i32, i32>, //should crash it
     }
 
+    #[web_fns]
     impl TestStruct {
+
         fn add(&self, other: i32) -> i32 {
             self.field + other
         }
@@ -22,7 +28,8 @@ mod tests {
     #[test]
     fn test() {
         let test = TestStruct {
-            field: 3
+            field: 3,
+            field2: HashMap::new(),
         };
 
         let k = test.add(5);
